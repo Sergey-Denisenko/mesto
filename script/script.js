f4890c8f1545eaebe4e4088e4f1454971fa86241
Sergey Denisenko <sd.a2@yandex.ru>
2020-05-23 21:46:10 +0300

    ПР5_сделаны удаление и лайки_начат попап картинки

--- script/script.js
+++ script/script.js
@@ -16,8 +16,10 @@ const cardImage = document.querySelector('.card__image'); // Картинка к
 const cardLike = document.querySelector('.card__like'); // Значек Like
 const popupAddCard = document.querySelector('.popup-add-card'); // Попап  AddCard
 const popupAddFormCloseButton = document.querySelector('.popup-add-card__form-close-button');
-const AddCardNameInput = document.querySelector('.popup-add-card__form-name');
-const AddCardImageLink = document.querySelector('.popup-add-card__form-image-link');
+const addCardNameInput = document.querySelector('.popup-add-card__form-name');
+const addCardImageLink = document.querySelector('.popup-add-card__form-image-link');
+const popupAddCardFormSubmit = document.querySelector('.popup-add-card__form-submit');
+
 
 const initialCards = [
   {
@@ -46,6 +48,64 @@ const initialCards = [
   }
 ];
 
+
+/*ДОБАВЛЯЮ КАРТОЧКИ НА СТРАНИЦУ*/
+  //Циклом добавляю карточки по умолчанию
+for (let i = 0; i < initialCards.length; i++) {
+  //Вызов функции добавления одной карточки
+  cardToPage(i);
+}
+/*ДОБАВЛЕНИЕ КАРТОЧКИ ПО УМОЛЧАНИЮ*/
+function cardToPage(evt) {
+  //Объявление константы, кладем в нее template карточки
+const cardTemplate = document.querySelector('#card').content;
+  //Объявление константы, клонирование содержимого template в переменную
+const cardElement = cardTemplate.cloneNode(true);
+  //Объявление константы для дальнейшей вставки карточки
+const cardContainer = document.querySelector('.card-container');
+  //Присвоение элементу карточки значения поля формы
+cardElement.querySelector('.card__title').textContent = initialCards[evt].name;
+  //Присвоение элементу карточки значения поля формы
+cardElement.querySelector('.card__image').src = initialCards[evt].link;
+console.log('Вставка массива карточек сработала');
+  //LIKE РВБОТА
+cardElement.querySelector('.card__like').addEventListener('click', function(evt) {
+  evt.target.classList.toggle('card__like_active-black');
+});
+  //УДАЛЕНИЕ КАРТОЧКИ
+cardElement.querySelector('.card__trash').addEventListener('click', function(evt) {
+  evt.target.closest('.card').remove();
+});
+  //ДОБАВЛЕНИЕ КАРТОЧКИ ПО УМОЛЧАНИЮ В КОНЕЦ ОЧЕРЕДИ / СПИСКА
+cardContainer.append(cardElement);
+}
+
+/*ФУНКЦИЯ ДОБАВЛЕНИЯ КАРТОЧКИ ПОЛЬЗОВАТЕЛЕМ*/
+function addInfo() {
+    //бъявление константы, кладем в нее template карточки
+  const cardTemplate = document.querySelector('#card').content;
+    //Объявление константы, клонирование содержимого template в переменную
+  const cardElement = cardTemplate.cloneNode(true);
+    //Объявление константы для дальнейшей вставки карточки
+  const cardContainer = document.querySelector('.card-container');
+    //Присвоение элементу карточки значения поля формы
+  cardElement.querySelector('.card__title').textContent = addCardNameInput.value;
+    //Присвоение элементу карточки значения поля формы
+  cardElement.querySelector('.card__image').src = addCardImageLink.value;
+  console.log('Вставка одной карточки сработала');
+    //LIKE РВБОТА
+  cardElement.querySelector('.card__like').addEventListener('click', function(evt) {
+    evt.target.classList.toggle('card__like_active-black');
+  });
+    //УДАЛЕНИЕ КАРТОЧКИ
+  cardElement.querySelector('.card__trash').addEventListener('click', function(evt) {
+    evt.target.closest('.card').remove();
+  });
+    //ДОБАВЛЕНИЕ КАРТОЧКИ ПОЛЬЗОВАТЕЛЕМ В НАЧАЛО ОЧЕРЕДИ / СПИСКА
+  cardContainer.prepend(cardElement);
+}
+
+
 /*ОТКРЫТИЕ И ЗАКРЫТИЕ ОКНА POPUP*/
 
 
@@ -54,35 +114,36 @@ function openPopup() {
   /* Присваиваю полям формы по умолчанию значения полей профиля  */
   nameInput.value = profileTitleName.textContent;
   jobInput.value = profileSubtitleAbout.textContent;
-    // console.log('Мы открыли popup');
     /* Добавляю модификатор класса popup*/
   popup.classList.add('popup_opened');
-    // console.log(popup.classList);
+  console.log('Мы открыли popup - openPopup');
+  // console.log(popup.classList);
 }
 
 /* Закрываю popup по нажатию на кнопку  */
 function closePopup() {
 /* Удаляю модификатор класса popup*/
   popup.classList.remove('popup_opened');
+  console.log('Мы закрыли popup - closePopup');
     // console.log(popup.classList);
 }
 
-
-
+/* Открываю popup по нажатию на кнопку  */
 function openPopupAddCard() {
-  /* Присваиваю полям формы по умолчанию значения полей профиля  */
-  // AddCardNameInput.value = cardTitle.textContent;
-  // AddCardImageLink.value = cardImage.src;
-    // console.log('Мы открыли popup');
-    /* Добавляю модификатор класса popup*/
-    popupAddCard.classList.add('popup_opened');
-    // console.log(popup.classList);
+  /* Запись пустой строки в поля формы */
+  addCardNameInput.value = '';
+  addCardImageLink.value = '';
+  /* Добавляю модификатор класса popup*/
+  popupAddCard.classList.add('popup-add-card_opened');
+  console.log('popupAddCard открыт');
+  // console.log(popup.classList);
 }
 
 /* Закрываю popup по нажатию на кнопку  */
 function closePopupAddCard() {
 /* Удаляю модификатор класса popup*/
-popupAddCard.classList.remove('popup_opened');
+  popupAddCard.classList.remove('popup-add-card_opened');
+  console.log('popupAddCard закрыт');
     // console.log(popup.classList);
 }
 
@@ -104,14 +165,20 @@ function formSubmitHandler (evt) {
     // Вставьте новые значения с помощью textContent
     profileTitleName.textContent = nameInput.value;
     profileSubtitleAbout.textContent = jobInput.value;
-
-
     // console.log(profileTitleName.textContent, 'Значение Имя')
     // console.log(profileSubtitleAbout.textContent, 'Значение Профессия')
     closePopup();
-    closePopupAddCard();
-    // console.log('Popup закрылся')
+    console.log('Работа функции formSubmitHandler закончена')
 }
+
+
+function formAddCardSubmitHandler (evt) {
+  evt.preventDefault();
+  addInfo();
+  closePopupAddCard();
+  console.log('Работа функции formAddCardSubmitHandler закончена')
+}
+
 popupFormSubmit.addEventListener('click', formSubmitHandler);
 popupFormSubmit.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openPopup);
@@ -119,9 +186,26 @@ popupFormCloseButton.addEventListener('click', closePopup);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 // formElement.addEventListener('submit', formSubmitHandler);
-
+// popupAddCardFormSubmit.addEventListener('click', formSubmitHandler);
+// popupAddCardFormSubmit.addEventListener('submit', formSubmitHandler);
+// profileAddButton.addEventListener('click', q);
 profileAddButton.addEventListener('click', openPopupAddCard);
 popupAddFormCloseButton.addEventListener('click', closePopupAddCard);
+popupAddCardFormSubmit.addEventListener('click', formAddCardSubmitHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -133,17 +217,18 @@ popupAddFormCloseButton.addEventListener('click', closePopupAddCard);
 //     форма должна закрываться по нажатию креста   ГОТОВО
 
 // - добавление карточки на страницу
-//     возможность задать в форме имя карточки
-//     возможность задать в форме дрес карточке для загрузки в контейнер
-//     при клике =сохранить= новая карточка должна встать на первое место, в начало контейнера
-//     при клике =сохранить= диалоговое окно должно закрываться
+//     по умолчанию встают 6 карточек из заданного массива   ГОТОВО
+//     возможность задать в форме имя карточки   ГОТОВО
+//     возможность задать в форме адрес карточке для загрузки в контейнер   ГОТОВО
+//     при клике =сохранить= новая карточка должна встать на первое место, в начало контейнера   ГОТОВО
+//     при клике =сохранить= диалоговое окно должно закрываться   ГОТОВО
 
-//     - возможность ставить лайки
-//         сердечко должно менять цвет
+//     - возможность ставить лайки   ГОТОВО
+//         сердечко должно менять цвет   ГОТОВО
 
-// - удаление карточки
+// - удаление карточки   ГОТОВО
 //     добавитть иконку корзины (удаления)   ГОТОВО
-//     карточка удаляется при клике на иконку удаления
+//     карточка удаляется при клике на иконку удаления   ГОТОВО
 
 // - открытие попапа с картинкой
 //     открытие попапа при нажатии на картинку
